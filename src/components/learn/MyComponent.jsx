48629bdc5285ef70ae3094f83cc468348287614c
Duongron1903 <duongron1903@gmail.com>
2025-05-12 16:04:14 +0700

    video30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,6 +1,13 @@
+
+//fragment
+
+import './style.css'
 const MyComponent = () => {
     return (
-        <div> Duong </div>
+        <>
+            <div> Duong </div>
+            <div className="child" style={{ borderRadius: "10px" }}>child</div>
+        </>
     );
 }
 
